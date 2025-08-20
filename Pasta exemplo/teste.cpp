336842b5f524b7fd07cb0102df4a3f499606f247
IsabelPessoa <isabelvaladarespessoa@gmail.com>
2025-08-20 19:34:29 -0300

    Teste de intalação do GCC no computador pessoal (Bem sucedido)

---
+++ Pasta exemplo/teste.cpp
@@ -0,0 +1,11 @@
+// Teste de instalação do MSYS2 (GCC)
+#include <iostream>
+
+int main(){
+    std::string helloworld = "Hello World!";
+    std::cout << helloworld << std::endl;
+    
+    std::cin.get(); 
+
+    return 0;
+}
